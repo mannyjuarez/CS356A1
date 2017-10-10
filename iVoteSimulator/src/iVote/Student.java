3fa94290b992947e7f7679d4b5fa750cf30f132f
Emanuel Juarez <erjuarez@cpp.edu>
2017-10-10 15:37:50 -0700

    10/10/17

---
+++ iVoteSimulator/src/iVote/Student.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iVote;
+
+/**
+ *
+ * @author mannyjuarez
+ */
+public class Student {
+    private String studentID;
+    
+    public void setStudentID(String ID)
+    {
+        studentID = ID;
+    }
+    
+    public String getStudentID()
+    {
+        return studentID;
+    }
+}
